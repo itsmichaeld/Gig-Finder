02d68d8edec430cf6310b10fb1473506dce6b30c
itsmichaeld <67594883+itsmichaeld@users.noreply.github.com>
2023-01-31 00:04:18 +0000

    Added favorite bands data collection
    Added the ability to collect and display a list of a users favorite bands pulled from the LastFM API

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// e9936476510a984d655a5af81258f7e8
+const gigs = document.getElementById("gigs");
+const username = document.getElementById("username");
+const searchBtn = document.getElementById("search-btn");
+const favouriteArtistsParagraph = document.getElementById("favourite-artists");
+let favouriteArtists = [];
 
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  fetch(
+    `http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username.value}&api_key=e9936476510a984d655a5af81258f7e8&limit=10&format=json`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      Object.values(data.topartists.artist).forEach((element) => {
+        favouriteArtistsParagraph.innerHTML += `<p>${element.name}</p>`;
+        favouriteArtists.push(element.name);
+      });
+    });
+});
